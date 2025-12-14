149a2aebeaaa9f78af8a00877b26f4db3e9036c9
younes <youneshssine@gmail.com>
2025-12-14 20:08:23 +0100

    fix: Corrected vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // AJOUTEZ CETTE LIGNE :
+  // Le chemin de base doit correspondre au nom de votre dépôt GitHub (/younes-portfolio/)
+  base: '/younes-portfolio/',
+
   plugins: [react()],
-})
+});
